ece134e81591d45093318a38778bc5020beacbee
quangminhtrancal <leap.ahead.tran@gmail.com>
2017-10-14 09:27:50 -0600

    Connect SocketIo

---
+++ Client/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Client
+//
+//  Created by Minh on 2017-10-14.
+//  Copyright © 2017 Minh. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
