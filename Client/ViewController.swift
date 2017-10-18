cce838266d060b38ca33f12db6c11476484c344f
quangminhtrancal <leap.ahead.tran@gmail.com>
2017-10-18 11:15:44 -0600

    Add the connection and the input and output text

--- Client/ViewController.swift
+++ Client/ViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 import SocketIO
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var txtout: UITextField!
+    
+    @IBOutlet weak var txtin: UITextField!
+    let socket = SocketIOClient(socketURL: URL(string: "http://localhost:2017")!, config: [.log(true), .compress])
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // CONNECT to HOST
+        socket.connect()
+        
+        socket.on("Data") { (data, ack) in
+            print(data)
+            self.txtout.text=data[0] as! String
+            
+        }
+        
     }
 
+    @IBAction func send(_ sender: Any) {
+        if (txtin.text != ""){
+            socket.emit("Name", with: [txtin.text!])
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
